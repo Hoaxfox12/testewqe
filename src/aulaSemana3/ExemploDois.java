46ef67220ab1169a01ec74299399e444d7f58dcc
Gustavo Oliveira <89104532+Hoaxfox12@users.noreply.github.com>
2024-05-02 19:26:56 -0300

    teste

---
+++ src/aulaSemana3/ExemploDois.java
@@ -0,0 +1,10 @@
+package aulaSemana3;
+
+public class ExemploDois {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/aulaSemana3/Main.java
@@ -0,0 +1,127 @@
+package aulaSemana3;
+
+import javax.swing.*;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		
+		
+		
+		//QUESTAO2
+		
+		/*
+		int num;
+		
+		num = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:  "));
+		
+		if(num % 5 == 0 && num % 3 == 0) {
+			JOptionPane.showMessageDialog(null, " O número   " + num + "é divisível por 3 e 5 ao mesmo tempo");
+		} else 
+			JOptionPane.showMessageDialog(null, "Ele não é divisível");
+		*/
+		
+		// questao 3
+		
+		/*
+		float a, b, c, d, n1, n2, n3, n4, soma;
+		
+		a = Float.parseFloat(JOptionPane.showInputDialog("Digite um número aleatório:  "));
+		b = Float.parseFloat(JOptionPane.showInputDialog("Digite um número aleatório:  "));
+		c = Float.parseFloat(JOptionPane.showInputDialog("Digite um número aleatório:  "));
+		d = Float.parseFloat(JOptionPane.showInputDialog("Digite um número aleatório:  "));
+		
+		
+		n1 = a;
+		
+		if(b<n1) {
+			n2 = n1;
+			n1 = b;		
+		} else  {
+			n2 = b;
+		} if (c < n1) {
+			n3 = n2;
+			n2 = n1;
+			n1 = c;
+		} else if (c < n2) {
+			n3 = n2;
+			n2 = c;
+		} else {
+			n3 = c;
+		}
+		if (d< n1) {
+			n4 = n3;
+			n3 = n2;
+			n2 = n1;
+			n1 = d;
+		} else if (d < n2) {
+			
+			n4 = n3;
+			n3 = n2;
+			n2 = d;
+		} else if (d <n3) {
+			n4 = n3;
+			n3 = d;
+		} else {
+			n4 = d;
+		}
+		
+		soma = n1 + n2 + n3;
+		
+		JOptionPane.showMessageDialog(null, soma);
+		*/
+		
+		// Questao 5
+		
+		/*
+		int mes;
+		
+		mes = Integer.parseInt(JOptionPane.showInputDialog("Digite um número para a conversão para mês:  "));
+		
+		
+		switch (mes) {
+		
+		case 1:
+		JOptionPane.showMessageDialog(null, "O mês é Janeiro"); 
+		break;
+		case 2:
+			JOptionPane.showMessageDialog(null, "O mês é Fevereiro"); 
+			break;
+		case 3:
+			JOptionPane.showMessageDialog(null, "O mês é Março"); 
+			break;
+		case 4:
+			JOptionPane.showMessageDialog(null, "O mês é Abril"); 
+			break;
+		case 5:
+			JOptionPane.showMessageDialog(null, "O mês é Maio");
+			break;
+		case 6:
+			JOptionPane.showMessageDialog(null, "O mês é Junho");
+			break;
+		case 7:
+			JOptionPane.showMessageDialog(null, "O mês é Julho"); 
+			break;
+		case 8:
+			JOptionPane.showMessageDialog(null, "O mês é Agosto"); 
+			break;
+		case 9:
+			JOptionPane.showMessageDialog(null, "O mês é Setembro"); 
+			break;
+		case 10:
+			JOptionPane.showMessageDialog(null, "O mês é Outubro"); 
+			break;
+		case 11:
+			JOptionPane.showMessageDialog(null, "O mês é Novembro"); 
+			break;
+		case 12:
+			JOptionPane.showMessageDialog(null, "O mês é Dezembro"); 
+			break;
+		default:
+			JOptionPane.showMessageDialog(null, "Não é um mês");
+		*/
+		
+		}
+		
+	}
+
